d215618d868ea9803157c971d817f233ca488d89
Антышев Дмитрий <Antyshev@mbp-antysev.dlink>
2016-02-11 22:19:18 -0800

    Initial Commit

---
+++ APIvkTest2(MY DZ)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APIvkTest2(MY DZ)
+//
+//  Created by Антышев Дмитрий on 11.02.16.
+//  Copyright © 2016 Антышев Дмитрий. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APIvkTest2(MY DZ)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APIvkTest2(MY DZ)
+//
+//  Created by Антышев Дмитрий on 11.02.16.
+//  Copyright © 2016 Антышев Дмитрий. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
